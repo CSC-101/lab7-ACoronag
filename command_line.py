e95eeb786e2d4fa1ef6b1ff96faa165b14c7d75f
Acoronag <acoronag@calpoly.edu>
2024-11-11 23:03:53 -0800

    Lab7

--- command_line.py
+++ command_line.py
@@ -1,1 +1,10 @@
+import sys
+def convert_float(parameter):
+    try:
+        parameter = float(parameter)
+    except ValueError:
+        print("not a number")
+    return parameter
 
+if __name__ == '__main__':
+    print(convert_float(sys.argv))
--- convert.py
+++ convert.py
@@ -1,1 +1,6 @@
+def str_to_float(string:str):
+    try:
+        return float(string)
+    except ValueError:
+        return None
 
